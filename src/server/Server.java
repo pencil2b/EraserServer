52947d4557085c250251781b3d4368d9f816e7a9
djosix <djosicks@gmail.com>
2016-12-30 00:50:46 +0800

    FUCK THE GOD

--- src/server/Server.java
+++ src/server/Server.java
@@ -9,26 +9,27 @@ import java.net.Socket;
 
 public class Server {
 
-
-
-    private final int PORT = 20000;
+    private static int port = 20000;
     private int count = 1;
 
     public static UDPSocket udp;
 
     public static void main(String args[]) throws IOException {
+        if (args.length > 1) {
+            port = Integer.parseInt(args[1]);
+        }
         new Server().establish();
     }
 
     public void establish() throws IOException {
 
-        udp = new UDPSocket(PORT);
+        udp = new UDPSocket(port);
         Data dd = new Data();
         Logic logic = new Logic();
         Threads threads = new Threads();
 
 
-        ServerSocket serverSock = new ServerSocket(PORT);
+        ServerSocket serverSock = new ServerSocket(port);
         while (true) {
             Socket cSocket = serverSock.accept();
             CDC.Data.addPlayer(count++,cSocket);
